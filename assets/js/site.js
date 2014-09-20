80e2a892f75b43ceb6c4d074d7c1186c2ec5f9a0
Scott Stadt <sstadt@fullsail.edu>
2014-09-19 23:11:10 -0500

    Added some libraries and updated readme

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,30 @@
-console.log(socket);
+$(function(){
+
+	// call to get classes
+	$.ajax({
+		type: 'GET',
+		url: '/static/classes',
+		dataType: 'json',
+		cache: false,
+		success: function (classes) {
+			console.log('I just fetched the classes:');
+			console.log('---------------------------------------');
+			console.log(classes);
+			console.log('');
+		}
+	});
+
+	// call to get weapons
+	$.ajax({
+		type: 'GET',
+		url: '/static/weapons',
+		dataType: 'json',
+		cache: false,
+		success: function (weapons) {
+			console.log('I just fetched the weapons:');
+			console.log('---------------------------------------');
+			console.log(weapons);
+			console.log('');
+		}
+	});
+});
